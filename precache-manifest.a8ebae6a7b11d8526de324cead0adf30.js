1334c4daf8a8096631398fe51f06ab94832692db
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-07 16:41:09 +0200

    Updates

---
+++ precache-manifest.a8ebae6a7b11d8526de324cead0adf30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d042926631bac4448b1c2cefed2c27",
+    "url": "/table-for-companies/index.html"
+  },
+  {
+    "revision": "f1ddd6b2977d014419f0",
+    "url": "/table-for-companies/static/css/main.70b51bc4.chunk.css"
+  },
+  {
+    "revision": "3ed3ad0c3107d3c79031",
+    "url": "/table-for-companies/static/js/2.c2c004f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/table-for-companies/static/js/2.c2c004f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ddd6b2977d014419f0",
+    "url": "/table-for-companies/static/js/main.f1d1ac87.chunk.js"
+  },
+  {
+    "revision": "2e377be5d94468f18ea2",
+    "url": "/table-for-companies/static/js/runtime-main.01a5aa18.js"
+  },
+  {
+    "revision": "4c461beb017664f8c41dd25d43941c01",
+    "url": "/table-for-companies/static/media/ring.4c461beb.svg"
+  }
+]);
